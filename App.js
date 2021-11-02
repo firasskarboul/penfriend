bfff758054617351143dda76c35030061ad30987
karboulx <fkarboul@gmail.com>
2021-11-02 21:57:19 +0100

    added firebase auth and app

--- App.js
+++ App.js
@@ -1,14 +1,13 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { ActivityIndicator } from 'react-native';
 import { Provider } from 'react-redux'
 import { store } from './redux/index';
 import { AppNavigation } from './components/AppNavigation';
 
 export default function App() {
-    return (
-      <Provider store={store}>
-        <AppNavigation />
-      </Provider>
-    );
+  return (
+    <Provider store={store}>
+      <AppNavigation />
+    </Provider>
+  );
 }
